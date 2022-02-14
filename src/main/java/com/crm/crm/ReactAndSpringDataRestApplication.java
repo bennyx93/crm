6a0febd6d2f81f8cd2913c8490c5ed11702d434d
Benny Chan <bennyx93@tamu.edu>
2022-02-14 14:55:51 -0600

    first commit

---
+++ src/main/java/com/crm/crm/ReactAndSpringDataRestApplication.java
@@ -0,0 +1,12 @@
+package com.crm.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactAndSpringDataRestApplication.class, args);
+    }
+}
